7d63fe1a727e1804f088f4b1a97c36b4c2a47365
hasankrdmn <hasankrduman@gmail.com>
2024-03-02 18:12:14 +0300

    stack.c updated

--- stack,queue,deque/stack/stack.c
+++ stack,queue,deque/stack/stack.c
@@ -10,85 +10,93 @@ typedef struct node{
 typedef struct {
     node *front;
     node *rear;
-}stack;
+}array;
 
 typedef struct {
-    stack *stack;
-}mystack;
+    array *stack;
+}stack;
 
 stack *createStack();
+array *createArray();
 void myStack_push(stack *obj, int data);
 int myStack_pop(stack *obj);
 void myStack_show(stack *obj);
 void myStack_free(stack *obj);
 
-// int main()
-// {
-//     stack *mystack = createStack();
-//     myStack_push(mystack, 1);
-//     myStack_push(mystack, 2);
-//     myStack_pop(mystack);
-//     myStack_push(mystack, 3);
-//     myStack_push(mystack, 4);
-//     myStack_pop(mystack);
-//     myStack_push(mystack, 5);
-//     myStack_show(mystack);
-//     myStack_free(mystack);
-// }
+int main()
+{
+    stack *mystack = createStack();
+    myStack_push(mystack, 1);
+    myStack_push(mystack, 2);
+    myStack_pop(mystack);
+    myStack_push(mystack, 3);
+    myStack_push(mystack, 4);
+    myStack_pop(mystack);
+    myStack_push(mystack, 5);
+    myStack_show(mystack);
+    myStack_free(mystack);
+}
 
 stack *createStack()
 {
-    stack *s = malloc(sizeof(stack));
-    s -> front = NULL;
-    s -> rear = NULL;
-    return (s);
+    stack *st = malloc(sizeof(stack));
+    st -> stack = createArray();
+    return (st);
+}
+
+array *createArray()
+{
+    array *arr = malloc(sizeof(array));
+    arr -> front = NULL;
+    arr -> rear = NULL;
+    return (arr);
 }
 
 void myStack_push(stack *obj, int data)
 {
     node *new_node = malloc(sizeof(node));
     new_node -> data = data;
-    new_node -> previous = obj -> rear;
+    new_node -> previous = obj -> stack -> rear;
     new_node -> next = NULL;
 
-    if (obj -> rear == NULL)
+    if (obj -> stack -> rear == NULL)
     {
         new_node -> previous = NULL;
-        obj -> front = new_node;
-        obj -> rear = new_node;
+        obj -> stack -> front = new_node;
+        obj -> stack -> rear = new_node;
         return;
     }
-    obj -> rear -> next = new_node;
-    obj -> rear = new_node;
+    obj -> stack -> rear -> next = new_node;
+    obj -> stack -> rear = new_node;
 }
 
 int myStack_pop(stack *obj)
 {
-    if (obj -> rear == NULL)
+    if (obj -> stack -> rear == NULL)
     {
         return (-999);
     }
-    int val = obj -> rear -> data;
-    node *tmp = obj -> rear;
-    obj -> rear = obj -> rear -> previous;
-    obj -> rear -> next = NULL;
+    int val = obj -> stack -> rear -> data;
+    node *tmp = obj -> stack -> rear;
+    obj -> stack -> rear = obj -> stack -> rear -> previous;
+    obj -> stack -> rear -> next = NULL;
     free(tmp);
     return (val);
 }
 
 void myStack_show(stack *obj)
 {
-    if (obj -> front == NULL)
+    if (obj -> stack -> front == NULL)
     {
         return;
     }
-    node *tmp = obj -> front;
-    while (obj -> front != NULL)
+    node *tmp = obj -> stack -> front;
+    while (obj -> stack -> front != NULL)
     {
-        printf("%d\n", obj->front->data);
-        obj -> front = obj -> front -> next;
+        printf("%d\n", obj -> stack -> front -> data);
+        obj -> stack -> front = obj -> stack -> front -> next;
     }
-    obj -> front = tmp;
+    obj -> stack -> front = tmp;
 }
 
 void myStack_free(stack *obj)
