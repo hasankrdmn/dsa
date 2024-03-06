f37dbd7a6a0cdfc1d7d7af314cd3fb9444b4c19e
hasankrdmn <hasankrduman@gmail.com>
2024-03-06 23:52:27 +0300

    fib.c & reverse_string.c

---
+++ trees_and_recursion/problem_solutions/reverse_string.c
@@ -0,0 +1,10 @@
+void reverseString(char* s, int sSize)
+{
+    char tmp;
+    for (int i = 0; i < sSize/2; i++)
+    {
+        tmp = s[i];
+        s[i] = s[sSize-i-1];
+        s[sSize-i-1] = tmp;
+    }
+}
